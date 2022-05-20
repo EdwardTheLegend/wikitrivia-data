3fb3e0b57845156737c9e33ff5aa4ae8fd181e92
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-05-20 10:10:49 +0800

    broaden country filter

--- country_filter.py
+++ country_filter.py
@@ -1,7 +1,10 @@
 import os
+import datetime
 
 item_filename = "items.json"
-countries_filename = "countries.json"
+countries_filename = (
+    "countries" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".json"
+)
 
 # check if countries.json exists and is not empty
 if os.path.isfile(countries_filename) and os.stat(countries_filename).st_size != 0:
@@ -12,7 +15,10 @@ with open(countries_filename, "a") as countries_file:
     # open items2.json
     with open(item_filename, "r") as f:
         for line in f:
+            # eval the line as a dict
+            item:dict = eval(line)
+            instance_of = item["instance_of"]
             # check if the current line is a country
-            if '"sovereign state"' in line:
+            if "country" in instance_of or "sovereign state" in instance_of or "nation" in instance_of:
                 # write the country to countries.json
                 countries_file.write(line)
